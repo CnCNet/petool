5633bf4b8cad0244c6ae41232c4bb2b7ba3eb078
Toni Spets <toni.spets@iki.fi>
2012-09-29 08:27:14 +0300

    Fix extpe file alignment and add support for section flags

--- extpe.c
+++ extpe.c
@@ -19,24 +19,54 @@
 #include <string.h>
 #include "pe.h"
 
+unsigned long bytealign(unsigned long raw, unsigned long align)
+{
+    return ((raw / align) + (raw % align > 0)) * align;
+}
+
 int main(int argc, char **argv)
 {
     FILE *fh;
     unsigned char *data;
-    unsigned int bytes = 0;
+    unsigned int bytes = 0x1000;
     long length;
     int i;
     unsigned int address = 0;
+    unsigned int flags = 0;
+    char *name = NULL;
 
     PIMAGE_DOS_HEADER dos_hdr;
     PIMAGE_NT_HEADERS nt_hdr;
     PIMAGE_SECTION_HEADER sct_hdr;
 
-    if (argc < 2)
+    if (argc < 5)
     {
-        fprintf(stderr, "usage: %s <executable> [bytes]\n", argv[0]);
+        fprintf(stderr, "usage: %s <executable> <section name> <flags: rwxciu> <size>\n", argv[0]);
         return 1;
     }
+
+    name = argv[2];
+
+    if (strlen(name) > 8)
+    {
+        fprintf(stderr, "Error: section name over 8 characters.\n");
+        return 1;
+    }
+
+    for (i = 0; i < strlen(argv[3]); i++)
+    {
+        switch (argv[3][i])
+        {
+            case 'r': flags |= IMAGE_SCN_MEM_READ;                  break;
+            case 'w': flags |= IMAGE_SCN_MEM_WRITE;                 break;
+            case 'x': flags |= IMAGE_SCN_MEM_EXECUTE;               break;
+            case 'c': flags |= IMAGE_SCN_CNT_CODE;                  break;
+            case 'i': flags |= IMAGE_SCN_CNT_INITIALIZED_DATA;      break;
+            case 'u': flags |= IMAGE_SCN_CNT_UNINITIALIZED_DATA;    break;
+        }
+    }
+
+    bytes = abs(atoi(argv[4]));
 
     fh = fopen(argv[1], "rb+");
     if (!fh)
@@ -64,13 +94,6 @@ int main(int argc, char **argv)
     nt_hdr = (void *)(data + dos_hdr->e_lfanew);
     sct_hdr = IMAGE_FIRST_SECTION(nt_hdr);
 
-    if (argc > 2)
-    {
-        bytes = abs(atoi(argv[2]));
-    }
-    if (bytes < 1) bytes = 1;
-    bytes = (bytes / nt_hdr->OptionalHeader.SectionAlignment + (bytes % nt_hdr->OptionalHeader.SectionAlignment ? 1 : 0)) * nt_hdr->OptionalHeader.SectionAlignment;
-
     printf("   section    start      end   length    vaddr  flags\n");
     printf("-----------------------------------------------------\n");
 
@@ -80,31 +103,41 @@ int main(int argc, char **argv)
     {
         if (i == nt_hdr->FileHeader.NumberOfSections - 1)
         {
-            strcpy((char *)sct_hdr->Name, ".patch");
+            /* for once, strncpy is useful */
+            strncpy((char *)sct_hdr->Name, name, 8);
             sct_hdr->Misc.VirtualSize = bytes;
             sct_hdr->VirtualAddress = address;
-            sct_hdr->SizeOfRawData = bytes;
-            sct_hdr->PointerToRawData = length;
-            sct_hdr->Characteristics = IMAGE_SCN_MEM_READ|IMAGE_SCN_MEM_WRITE|IMAGE_SCN_MEM_EXECUTE|IMAGE_SCN_CNT_CODE;
+            sct_hdr->SizeOfRawData = bytealign(bytes, nt_hdr->OptionalHeader.FileAlignment);
+            sct_hdr->PointerToRawData = flags & IMAGE_SCN_CNT_UNINITIALIZED_DATA ? 0 : length;
+            sct_hdr->Characteristics = flags;
 
-            nt_hdr->OptionalHeader.SizeOfCode               += bytes;
-            nt_hdr->OptionalHeader.SizeOfImage              += bytes;
-            nt_hdr->OptionalHeader.SizeOfInitializedData    += bytes;
-            nt_hdr->OptionalHeader.CheckSum                 = 0x00000000; /* do I really need to recalc this? */
-        }
-        else
+            /* for expanding the image file */
+            bytes = sct_hdr->SizeOfRawData;
+
+            if (flags & IMAGE_SCN_CNT_CODE)
+                nt_hdr->OptionalHeader.SizeOfCode               += sct_hdr->SizeOfRawData;
+
+            if (flags & IMAGE_SCN_CNT_INITIALIZED_DATA)
+                nt_hdr->OptionalHeader.SizeOfInitializedData    += sct_hdr->SizeOfRawData;
+
+            if (flags & IMAGE_SCN_CNT_UNINITIALIZED_DATA)
+                nt_hdr->OptionalHeader.SizeOfUninitializedData  += sct_hdr->SizeOfRawData;
+
+            nt_hdr->OptionalHeader.SizeOfImage  = bytealign(sct_hdr->VirtualAddress +  sct_hdr->SizeOfRawData, nt_hdr->OptionalHeader.SectionAlignment);
+            nt_hdr->OptionalHeader.CheckSum     = 0x00000000;
+        } else
         {
             if (sct_hdr->VirtualAddress >= address)
             {
-                address = ((sct_hdr->VirtualAddress + sct_hdr->SizeOfRawData) / nt_hdr->OptionalHeader.SectionAlignment + ((sct_hdr->VirtualAddress + sct_hdr->SizeOfRawData) % nt_hdr->OptionalHeader.SectionAlignment ? 1 : 0)) * nt_hdr->OptionalHeader.SectionAlignment;
+                address = sct_hdr->VirtualAddress + bytealign(sct_hdr->Misc.VirtualSize ? sct_hdr->Misc.VirtualSize : sct_hdr->SizeOfRawData, nt_hdr->OptionalHeader.SectionAlignment);
             }
         }
 
-        printf("%10s %8d %8d %8d %8X %s%s%s%s%s%s %s\n",
+        printf("%10.8s %8d %8d %8d %8X %s%s%s%s%s%s %s\n",
                 sct_hdr->Name,
                 sct_hdr->PointerToRawData,
                 sct_hdr->PointerToRawData + sct_hdr->SizeOfRawData,
-                sct_hdr->SizeOfRawData,
+                sct_hdr->SizeOfRawData ? sct_hdr->SizeOfRawData : sct_hdr->Misc.VirtualSize,
                 sct_hdr->VirtualAddress + nt_hdr->OptionalHeader.ImageBase,
                 sct_hdr->Characteristics & IMAGE_SCN_MEM_READ               ? "r" : "-",
                 sct_hdr->Characteristics & IMAGE_SCN_MEM_WRITE              ? "w" : "-",
@@ -119,11 +152,16 @@ int main(int argc, char **argv)
     }
 
     fwrite(data, length, 1, fh);
-
-    memset(data, 0x00, bytes);
-    fwrite(data, bytes, 1, fh);
-
     free(data);
+
+    /* only expand image if real data */
+    if (!(flags & IMAGE_SCN_CNT_UNINITIALIZED_DATA))
+    {
+        data = calloc(1, bytes);
+        fwrite(data, bytes, 1, fh);
+        free(data);
+    }
+
     fclose(fh);
 
     return 0;

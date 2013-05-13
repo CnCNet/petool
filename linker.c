bac0629c4d0fa70c2e9d75960b6a07f872d815df
John Ericson <Ericson2314@Yahoo.com>
2013-05-13 16:17:39 -0400

    WIN32_LEAN_AND_MEAN is usually a good idea

--- linker.c
+++ linker.c
@@ -20,6 +20,7 @@
 #include <strings.h>
 #include <ctype.h>
 #include <unistd.h>
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include "list.h"
 
--- modpe.c
+++ modpe.c
@@ -17,6 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 int main(int argc, char **argv)

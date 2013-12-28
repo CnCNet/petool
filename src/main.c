3d7dcfdeaecb8154a5d6e83f26b568eb457b58f2
Toni Spets <toni.spets@iki.fi>
2013-12-28 19:53:16 +0200

    setvs - Change VirtualSize of a section

--- src/main.c
+++ src/main.c
@@ -23,6 +23,7 @@ int genlds(int argc, char **argv);
 int pe2obj(int argc, char **argv);
 int patch(int argc, char **argv);
 int setdd(int argc, char **argv);
+int setvs(int argc, char **argv);
 
 void help(char *progname)
 {
@@ -34,6 +35,7 @@ void help(char *progname)
             "    pe2obj -- convert executable into object file"                 "\n"
             "    patch  -- perform patches from .patch section"                 "\n"
             "    setdd  -- set any DataDirectory in PE header"                  "\n"
+            "    setvs  -- set VirtualSize for a section"                       "\n"
             "    help   -- this information"                                    "\n"
     );
 }
@@ -51,6 +53,7 @@ int main(int argc, char **argv)
     else if (strcmp(argv[1], "pe2obj") == 0) return pe2obj (argc - 1, argv + 1);
     else if (strcmp(argv[1], "patch")  == 0) return patch  (argc - 1, argv + 1);
     else if (strcmp(argv[1], "setdd")  == 0) return setdd  (argc - 1, argv + 1);
+    else if (strcmp(argv[1], "setvs")  == 0) return setvs  (argc - 1, argv + 1);
     else if (strcmp(argv[1], "help")   == 0)
     {
         help(argv[0]);
---
+++ src/setvs.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2013 Toni Spets <toni.spets@iki.fi>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <unistd.h>
+
+#include "pe.h"
+#include "cleanup.h"
+#include "common.h"
+
+int setvs(int argc, char **argv)
+{
+    // decleration before more meaningful initialization for cleanup
+    int     ret   = EXIT_SUCCESS;
+    FILE   *fh    = NULL;
+    int8_t *image = NULL;
+
+    NO_FAIL(argc < 4, "usage: petool setvs <image> <section> <VirtualSize>\n");
+
+    uint32_t vs   = strtol(argv[3], NULL, 0);
+
+    uint32_t length;
+    NO_FAIL_SILENT(open_and_read(&fh, &image, &length, argv[1], "r+b"));
+
+    PIMAGE_DOS_HEADER dos_hdr = (void *)image;
+    PIMAGE_NT_HEADERS nt_hdr = (void *)(image + dos_hdr->e_lfanew);
+
+    NO_FAIL(length < sizeof (IMAGE_DOS_HEADER),         "File too small.\n");
+    NO_FAIL(dos_hdr->e_magic != IMAGE_DOS_SIGNATURE,    "File DOS signature invalid.\n");
+    NO_FAIL(dos_hdr->e_lfanew == 0,                     "NT header missing.\n");
+    NO_FAIL(nt_hdr->Signature != IMAGE_NT_SIGNATURE,    "File NT signature invalid.\n");
+    NO_FAIL(vs < 1,                                     "VirtualSize can't be zero.\n");
+
+    for (int32_t i = 0; i < nt_hdr->FileHeader.NumberOfSections; i++)
+    {
+        PIMAGE_SECTION_HEADER sct_hdr = IMAGE_FIRST_SECTION(nt_hdr) + i;
+
+        if (strcmp(argv[2], (char *)sct_hdr->Name) == 0)
+        {
+            NO_FAIL(vs < sct_hdr->SizeOfRawData,"VirtualSize can't be smaller than raw size.\n");
+            sct_hdr->Misc.VirtualSize = vs;
+            vs = 0;
+            break;
+        }
+    }
+
+    NO_FAIL(vs != 0, "No '%s' section in given PE image.\n", argv[2]);
+
+    /* FIXME: implement checksum calculation */
+    nt_hdr->OptionalHeader.CheckSum = 0;
+
+    rewind(fh);
+    NO_FAIL_PERROR(fwrite(image, length, 1, fh) != 1, "Error writing executable");
+
+cleanup:
+    if (image) free(image);
+    if (fh)    fclose(fh);
+    return ret;
+}

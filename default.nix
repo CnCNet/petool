e2ef2778b9f5860b0fbce503121f95346bffc65b
John Ericson <Ericson2314@Yahoo.com>
2014-08-10 00:39:00 +0200

    Add a basic nix package expression

---
+++ default.nix
@@ -0,0 +1,26 @@
+{stdenv ? (import <nixpkgs> {}).stdenv }:
+
+stdenv.mkDerivation rec {
+  name = "petool";
+  version = "1.1.1.1";
+
+  buildInputs = [ ];
+  src = ./.;
+  preBuild = "makeFlagsArray=(REV=${version})";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -m 755 petool $out/bin
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    homepage = http://github.com/cnc-patch/petool;
+    description = "Tool to help rebuild, extend and patch 32-bit Windows applications.";
+    # maintainers =
+    license = map (builtins.getAttr "shortName") [ licenses.mit ];
+    # Should be portable C
+    platforms = stdenv.lib.platforms.all;
+  };
+}

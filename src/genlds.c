a3b7418c527f515b25baf76f8fe3b210b396dbfa
Toni Spets <toni.spets@iki.fi>
2015-04-26 20:03:25 +0300

    Automatic .bss generation for genlds

--- src/genlds.c
+++ src/genlds.c
@@ -19,6 +19,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <inttypes.h>
 #include <ctype.h>
 #include <unistd.h>
@@ -56,16 +57,43 @@ int genlds(int argc, char **argv)
     printf("SECTIONS\n");
     printf("{\n");
 
+    bool idata_exists = false;
+    char bss_name[10] = { '\0' };
+
     for (int i = 0; i < nt_hdr->FileHeader.NumberOfSections; i++)
     {
         const PIMAGE_SECTION_HEADER cur_sct = IMAGE_FIRST_SECTION(nt_hdr) + i;
         char buf[9];
         memset(buf, 0, sizeof buf);
         memcpy(buf, cur_sct->Name, 8);
+
+        if (cur_sct->Characteristics & IMAGE_SCN_CNT_UNINITIALIZED_DATA) {
+            printf("    %-15s   0x%-6"PRIX32" : { . = . + 0x%"PRIX32"; }\n", buf, cur_sct->VirtualAddress + nt_hdr->OptionalHeader.ImageBase, cur_sct->Misc.VirtualSize ? cur_sct->Misc.VirtualSize : cur_sct->SizeOfRawData);
+            strcpy(bss_name, buf);
+            continue;
+        }
+
         printf("    %-15s   0x%-6"PRIX32" : { %s(%s) }\n", buf, cur_sct->VirtualAddress + nt_hdr->OptionalHeader.ImageBase, argv[1], buf);
+
+        if (cur_sct->Misc.VirtualSize > cur_sct->SizeOfRawData) {
+            printf("    .bss         ALIGN(0x%-4"PRIX32") : { . = . + 0x%"PRIX32"; }\n", nt_hdr->OptionalHeader.SectionAlignment, cur_sct->Misc.VirtualSize - cur_sct->SizeOfRawData);
+        }
+
+        if (strcmp(buf, ".idata") == 0) {
+            idata_exists = true;
+        }
     }
 
     printf("\n");
+
+    if (!idata_exists) {
+        printf("    .idata       ALIGN(0x%-4"PRIX32") : { *(.idata); }\n\n", nt_hdr->OptionalHeader.SectionAlignment);
+    }
+
+    if (bss_name[0]) {
+        printf("    /DISCARD/                  : { %s(%s) }\n", argv[1], bss_name);
+    }
+
     printf("    /DISCARD/                  : { *(.drectve) }\n");
     printf("    .p_text      ALIGN(0x%-4"PRIX32") : { *(.text); }\n", nt_hdr->OptionalHeader.SectionAlignment);
     printf("    .p_rdata     ALIGN(0x%-4"PRIX32") : { *(.rdata); }\n", nt_hdr->OptionalHeader.SectionAlignment);
